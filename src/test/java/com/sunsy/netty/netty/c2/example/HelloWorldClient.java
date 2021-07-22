3a1e319f9c2f0577ce61db0272f8589a3b6a12eb
sunsy <1587046322@qq.com>
2021-07-23 00:12:13 +0800

    【需求描述】: 将本地Netty笔记上传至github

---
+++ src/test/java/com/sunsy/netty/netty/c2/example/HelloWorldClient.java
@@ -0,0 +1,45 @@
+package com.sunsy.netty.netty.c2.example;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorldClient {
+
+    private static Logger log = LoggerFactory.getLogger(HelloWorldClient.class);
+
+    public static void main(String[] args) {
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.group(worker);
+            bootstrap.handler(new ChannelInitializer<NioSocketChannel>() {
+                @Override
+                protected void initChannel(NioSocketChannel channel) throws Exception {
+                    channel.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                        // 会在channel建立成功后，触发active事件
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            for (int i = 0; i < 10; i ++) {
+                                ByteBuf byteBuf = ctx.alloc().buffer(16);
+                                byteBuf.writeBytes(new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
+                                ctx.writeAndFlush(byteBuf);
+                            }
+                        }
+                    });
+                }
+            });
+            ChannelFuture channelFuture = bootstrap.connect("localhost", 8080).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            log.error("client error = {}", e);
+        } finally {
+            worker.shutdownGracefully();
+        }
+    }
+}
---
+++ src/test/java/com/sunsy/netty/netty/c2/example/HelloWroldServer.java
@@ -0,0 +1,48 @@
+package com.sunsy.netty.netty.c2.example;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.AdaptiveRecvByteBufAllocator;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 演示黏包半包现象
+ */
+@Slf4j
+public class HelloWroldServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            // 设置接收缓冲区大小，展示半包问题，10表示10个字节
+            // 这个就是滑动窗口大小，不设置的话自适应，一般无需手动调整
+            serverBootstrap.option(ChannelOption.SO_RCVBUF, 10);
+            // 设置netty的接收缓冲区(byteBuf)
+            // serverBootstrap.childOption(ChannelOption.RCVBUF_ALLOCATOR,
+            //      new AdaptiveRecvByteBufAllocator(16, 16, 16));
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
+                @Override
+                protected void initChannel(NioSocketChannel channel) throws Exception {
+                    channel.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind(8080).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+}
